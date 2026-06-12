e0879e93df0ae60b4799553f799ae63aae3c127e
ManazirHasanKhan <mkhanj2ee@gmail.com>
2026-06-12 20:47:16 +0300

    commit1

---
+++ src/aug2225/ds/DsTest.java
@@ -0,0 +1,8 @@
+package aug2225.ds;
+
+public class DsTest {
+
+    public static void main(String[] args) {
+        System.out.println("----------BisMilAllah-InShaAllah-MaaShaAllah----------");
+    }
+}
